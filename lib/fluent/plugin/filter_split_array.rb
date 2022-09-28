4f58deee168c0daeefe5454d1dc9fcde59a6e589
길세현 <gil9084@gmail.com>
2022-09-28 16:06:05 +0900

    spread json

--- lib/fluent/plugin/filter_split_array.rb
+++ lib/fluent/plugin/filter_split_array.rb
@@ -11,17 +11,32 @@ module Fluent::Plugin
       new_es = Fluent::MultiEventStream.new
       es.each {|time, record|
         target_record = @split_key.nil? ? record : record[@split_key] || {}
-        split(time, target_record, new_es)
+        distributed_record = {}
+        if !@split_key.nil?
+          record.each_pair do |k, v|
+            if k == @split_key
+              next
+            end
+            distributed_record[k] = v
+          end
+        end 
+        split(time, target_record, new_es, distributed_record)
       }
       new_es
     end
 
     private
 
-    def split(time, record, new_es)
+    def split(time, record, new_es, distributed_record)
       if record.instance_of?(Array)
+        distributed_record.each_pair do |k, v|
+          record.each { |r| r[k] = v}
+        end
         record.each { |r| new_es.add(time, r) }
       else
+        distributed_record.each_pair do |k, v|
+          record[k] = v
+        end
         new_es.add(time, record)
       end
     end
